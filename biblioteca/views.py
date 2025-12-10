cd2c9ed0c962dac6b116394aef993fc28f14965c
FrancisFernandez-dev <fra.ferley@gmail.com>
2025-12-09 21:52:21 -0300

    implementar CRUD completo para modelos 3D

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, redirect
-from .models import Model3D
-from .forms import Model3DForm
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Model3D, Category
+from .forms import Model3DForm
+
 
 def inicio(request):
     return render(request, 'biblioteca/inicio.html')
 
 
 def lista_modelos(request):
-    modelos = Model3D.objects.all()
+    modelos = Model3D.objects.all().select_related('categoria').prefetch_related('etiquetas')
     return render(request, 'biblioteca/lista_modelos.html', {'modelos': modelos})
 
 
@@ -25,13 +26,22 @@ def agregar_modelo(request):
 
 
 @login_required
-def agregar_modelo(request):
+def editar_modelo(request, pk):
+    modelo = get_object_or_404(Model3D, pk=pk)
     if request.method == 'POST':
-        form = Model3DForm(request.POST)
+        form = Model3DForm(request.POST, instance=modelo)
         if form.is_valid():
             form.save()
             return redirect('lista_modelos')
     else:
-        form = Model3DForm()
-    return render(request, 'biblioteca/agregar_modelo.html', {'form': form})
+        form = Model3DForm(instance=modelo)
+    return render(request, 'biblioteca/editar_modelo.html', {'form': form, 'modelo': modelo})
 
+
+@login_required
+def eliminar_modelo(request, pk):
+    modelo = get_object_or_404(Model3D, pk=pk)
+    if request.method == 'POST':
+        modelo.delete()
+        return redirect('lista_modelos')
+    return render(request, 'biblioteca/confirmar_eliminar.html', {'modelo': modelo})
